846bd7ddeff7ae38fc1ed3f59f3fb3263358bc15
Andy Ma <machao.andy@gmail.com>
2026-09-13 22:26:02 +0800

    x

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,8 +4,13 @@ return {
 	priority = 1000,
 	config = function()
 		require("tokyonight").setup({
+			style = "moon",
 			transparent = true,
+			styles = {
+				sidebars = "transparent",
+				floats = "transparent",
+			},
 		})
-		vim.cmd([[colorscheme tokyonight]])
+		vim.cmd.colorscheme("tokyonight")
 	end,
 }
